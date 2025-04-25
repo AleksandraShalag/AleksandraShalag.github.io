4508383a1c807c93064d21ae094825ca71bc992a
AleksandraShalag <139496865+AleksandraShalag@users.noreply.github.com>
2025-04-25 12:56:13 +0300

    feat(task-list-component)
    feat(task-list-component):
    - Сделан элемент задачи перетаскиваемым (draggable)
    - Добавлены обработчики событий dragstart и dragend
    - Добавлен приватный метод #afterCreateElement для инициализации перетаскивания

--- src/view/task-list-compoment.js
+++ src/view/task-list-compoment.js
@@ -1,28 +1,48 @@
 import {createElement} from '../framework/render.js'; 
 import { AbstractComponent } from '../framework/view/abstract-component.js';
 
-
-function createTaskListComponentTemplate({status,task}) {
-    return (
-      `<li class="task-list-item-${status.class} id=${task.id}">${task.title}</li>`
-    );
-  }
-
+function createTaskListComponentTemplate({ status, task }) {
+  return (`
+    <li class="task-list-item ${status.class}" data-id="${task.id}">${task.title}</li>`
+  );
+}
 
 export default class TaskListComponent extends AbstractComponent{
-
-  constructor({status, task}) {
-    super(); // Обязательный вызов родительского конструктора
+  constructor({status, task, onDragOver}) {
+    super();
     this._status = status;
     this._task = task;
-    this._element = null; // Приватное свойство вместо публичного
+    this._onDragOver = onDragOver; // Колбэк из родителя
+    this.element.dataset.id = task.id;
+    this.#afterCreateElement();
   }
-  
+
+  #afterCreateElement(){
+    this.#makeTaskDraggable();
+  }
+
   get template() {
     return createTaskListComponentTemplate({
       status: this._status,
       task: this._task,
     });
+  }
+
+  #makeTaskDraggable(){
+    this.element.setAttribute('draggable', true);
+  
+    this.element.addEventListener('dragstart', (event) => {
+      this.element.classList.add('dragging');
+      event.dataTransfer.setData('text/plain', this._task.id);
+    });
+  
+    this.element.addEventListener('dragend', () => {
+      this.element.classList.remove('dragging');
+    });
+  }
+
+  setListElements(elements) {
+    this._listElements = elements;
   }
 
 }
