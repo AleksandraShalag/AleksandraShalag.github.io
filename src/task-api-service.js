5be5210de97f9c188d170b76fc6dcf987c91b6fa
AleksandraShalag <139496865+AleksandraShalag@users.noreply.github.com>
2025-05-01 23:03:45 +0300

    feat(task-api-service)

---
+++ src/task-api-service.js
@@ -0,0 +1,54 @@
+import ApiService from "./framework/view/api-service.js";
+
+
+const Method = {
+ GET: 'GET',
+ PUT: 'PUT',
+ POST: 'POST',
+ DELETE: 'DELETE',
+};
+
+
+export default class TasksApiService extends ApiService {
+
+  async getTasks() {
+    const response = await this._load({url: 'tasks'});
+    return ApiService.parseResponse(response);
+  }
+
+  async addTask(task) {
+    const response = await this._load({
+      url: 'tasks',
+      method: Method.POST,
+      body: JSON.stringify(task),
+      headers: new Headers({ 'Content-Type': 'application/json' })
+    });
+    return ApiService.parseResponse(response);
+  }
+
+  async updateTask(task) {
+    const response = await this._load({
+      url: `tasks/${task.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(task),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+ 
+    const parsedResponse = await ApiService.parseResponse(response);
+ 
+    return parsedResponse;
+  
+}
+
+   
+async deleteTask(taskId) {
+  await this._load({
+    url: `tasks/${taskId}`,
+    method: Method.DELETE,
+  });
+}
+      
+     
+     
+
+}
